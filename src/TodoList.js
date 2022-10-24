b004549df7ff0cd16e277854e663d667430ada2d
Huy Trần <trantienhuy1112@gmail.com>
2022-10-25 00:38:12 +0700

    test CI_1

---
+++ src/TodoList.js
@@ -0,0 +1,46 @@
+import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
+import { useContext} from "react";
+import { NewTask } from "./App";
+
+const TodoList = () => {
+  const ctxTask = useContext(NewTask);
+
+  const handleClick = (id) => {
+    const newTask = ctxTask.task;
+    for (let i = 0; i < newTask.length; i++) {
+      if (newTask[i].id === id) {
+        newTask[i].check = !newTask[i].check;
+      }
+    }
+    ctxTask.setTask([...newTask]);
+  };
+  return (
+    <div className="todo-list-container">
+      {ctxTask.task.map((item) => {
+        return item.check == true ? (
+          <div className="todo-item-container">
+            <FaRegCircle
+              className="item-done-button"
+              color="#9a9a9a"
+              onClick={() => handleClick(item.id)}
+            />
+            <div className="item-title">{item.name}</div>
+          </div>
+        ) : ctxTask.show == true ? (
+          <div className="todo-item-container done">
+            <FaRegCircle
+              className="item-done-button"
+              color="#9a9a9a"
+              onClick={() => handleClick(item.id)}
+            />
+            <div className="item-title">{item.name}</div>
+          </div>
+        ) : (
+          ""
+        );
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
